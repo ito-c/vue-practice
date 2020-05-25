3af9a52623c8dc99ac1d8a823b96acd9a5cc7258
tashi1118 <tech.tashi1118.1991@gmail.com>
2020-05-26 01:10:02 +0900

    条件付きレンダリング

---
+++ sec9-if-else/js/main.js
@@ -0,0 +1,23 @@
+new Vue({
+    el: '#app',
+    data: {
+        toggle: false
+    }
+})
+
+
+new Vue({
+    el: '#appElse',
+    data: {
+        color: 'orange'
+    }
+})
+
+
+
+new Vue({
+    el: '#appShow',
+    data: {
+        toggle: false
+    }
+})
