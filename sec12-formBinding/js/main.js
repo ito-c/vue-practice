a478d98275c7c7a6c85c2c7d9b308f036bdce2ab
tashi1118 <tech.tashi1118.1991@gmail.com>
2020-05-26 12:02:38 +0900

    フォーム入力関連バインディング

---
+++ sec12-formBinding/js/main.js
@@ -0,0 +1,69 @@
+new Vue({
+    el: '#appA',
+    data: {
+        message: 'hello vue'
+    }
+})
+
+new Vue({
+    el: '#appB',
+    data: {
+        message: 'hogehoge'
+    }
+})
+
+new Vue({
+    el: '#appC',
+    data: {
+        checked: false
+    }
+})
+
+
+new Vue({
+    el: '#appD',
+    data: {
+        colors: []
+    }
+})
+
+
+new Vue({
+    el: '#appE',
+    data: {
+        color: '' // ラジオボタンのデフォルトの値の型は文字列
+    }
+})
+
+
+new Vue({
+    el: '#appF',
+    data: {
+        selected: '' // セレクトボタン単体選択のデフォルトの値の型は文字列
+    }
+})
+
+
+new Vue({
+    el: '#appG',
+    data: {
+        selectedMulti: [] // セレクトボタン複数選択のデフォルトの値の型は配列
+    }
+})
+
+
+new Vue({
+    el: '#appI',
+    data: {
+        message: ''
+    }
+})
+
+
+
+new Vue({
+    el: '#appJ',
+    data: {
+        age: 0
+    }
+})
